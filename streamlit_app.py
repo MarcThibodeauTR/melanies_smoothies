8e7e6ff15169fde93576edce55c531318c7f9c7b
Marc Thibodeau <marc.thibodeau@thomsonreuters.com>
2025-07-30 12:51:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,6 @@ if ingredients_list:
 
 #New section to display smoothiefruit nutrition informaiton
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("http://www.fruityvice.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
